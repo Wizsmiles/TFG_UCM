743df329a255590f561c9aea76b9d98b5c5d328e
serulloa <sergio95rlc@gmail.com>
2018-03-06 00:07:37 +0100

    Update Recorridos class
    Se ha añadido el algoritmo de recorrido Divide & Query, que siempre entra en el hijo cuyo número de nodos se acerque más a la mitad del número de nodos del padre. Se ha probado y parece que funciona correctamente.

--- TFG/Main/Recorridos.py
+++ TFG/Main/Recorridos.py
@@ -5,6 +5,7 @@ import sys
 class Estrategia(Enum):
     TOPDOWN = 1
     HEAVIESTFIRST = 2
+    DIVIDEANDQUERY = 3
 
 class Recorrido():
     def __init__(self, tree):
@@ -58,6 +59,45 @@ class Recorrido():
         else:
             self.buggy = True;
 
+
+    def inicializarDQ(self):
+        self.divideAndQuery(self.arbol)
+
+    def divideAndQuery(self, nodo):
+        if len(nodo.hijos) != 0:
+            descendientes = []
+            validos = 0
+
+            for i in nodo.hijos:
+                if i.estado == Nodo.Estado.INDEFINIDO:
+                    descendientes.append(abs(nodo.nNodos/2 - i.nNodos))
+                elif i.estado == Nodo.Estado.VALIDO or i.estado == Nodo.Estado.CONFIAR:
+                    validos = validos + 1
+
+            print(descendientes)
+
+            if len(nodo.hijos) == validos:
+                self.buggy = True
+            elif len(descendientes) != 0:
+                found = False
+                j = 0
+
+                while (found == False):
+                    n = abs(nodo.nNodos/2 - nodo.hijos[j].nNodos)
+
+                    if n == min(descendientes) and nodo.hijos[j].estado == Nodo.Estado.INDEFINIDO:
+                        found = True
+                        self.ask(nodo.hijos[j])
+
+                        if(nodo.hijos[j].estado == Nodo.Estado.VALIDO or nodo.hijos[j].estado == Nodo.Estado.CONFIAR):
+                            self.divideAndQuery(nodo)
+
+                    j = j + 1
+
+            else:
+                self.buggy = True
+
+
     def ask(self, nodo):
             print("Soy la funcion:", nodo.getNombre())
             #print("Mi Id es:", self.id)
@@ -66,7 +106,7 @@ class Recorrido():
             print("Valor retornado por '"+nodo.getNombre()+"' es:", nodo.getValor())
             nb=""
             while(nb!="y" and nb!="n" and nb!="t"):
-                nb = input("It's correct?(y/n/t) t for trust")
+                nb = input("It's correct?(y/n/t) t for trust: ")
                 print(nb)
 
             if(nb == "y"):
@@ -86,6 +126,8 @@ class Recorrido():
                     self.topDown(nodo)
                 if(self.estrategia == Estrategia.HEAVIESTFIRST):
                     self.heaviestFirst(nodo)
+                if(self.estrategia == Estrategia.DIVIDEANDQUERY):
+                    self.divideAndQuery(nodo)
             if(self.buggy):
                 print("Buggy en la función:", nodo.getNombre())
                 print("Valor retornado es:", nodo.getValor())
