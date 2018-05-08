aae4ff3ebc79b5aa249d516eaabdcb2b8527163f
Jose Javier Escudero Gómez <doublejay93@gmail.com>
2018-05-08 19:35:01 +0200

    New examples
    Añadidos como ejemplos, los algoritmos de quicksort y mergesort

--- TFG/Main/Utils/Ejemplos.py
+++ TFG/Main/Utils/Ejemplos.py
@@ -80,5 +80,78 @@ def excepcion3():
     excepcion1()
 
 def ejemplo4():
-   excepcion3()
-   excepcion2()
+    excepcion3()
+    excepcion2()
+   
+## EJEMPLOS OBTENIDOS DE ROSETTACODE ##
+
+# Codigo ejemplo de QuickSort
+
+def myQuickSort():
+    array = [3,6,2,7,1]
+    quickSort(array)
+    
+    for element in array:
+        print(element + "\n")
+    
+def quickSort(arr):
+    less = []
+    pivotList = []
+    more = []
+    
+    if len(arr) <= 1:
+        return arr
+    
+    else:
+        pivot = arr[0]
+        for i in arr:
+            if i < pivot:
+                less.append(i)
+            elif i > pivot:
+                more.append(i)
+            else:
+                pivotList.append(i)
+                
+        less = quickSort(less)
+        more = quickSort(more)
+        return less + pivotList + more
+    
+# Codigo ejemplo de MergeSort
+
+def myMergeSort():
+    array = [3,6,2,7,1]
+    mergeSort(array)
+    
+    for element in array:
+        print(element + "\n")
+     
+def mergeSort(m):
+    if len(m) <= 1:
+        return m
+ 
+    middle = len(m) // 2
+    left = m[:middle]
+    right = m[middle:]
+ 
+    left = mergeSort(left)
+    right = mergeSort(right)
+    return list(merge(left, right))
+    
+def merge(left, right):
+    result = []
+    left_idx, right_idx = 0, 0
+    while left_idx < len(left) and right_idx < len(right):
+        # change the direction of this comparison to change the direction of the sort
+        if left[left_idx] <= right[right_idx]:
+            result.append(left[left_idx])
+            left_idx += 1
+        else:
+            result.append(right[right_idx])
+            right_idx += 1
+ 
+    if left_idx < len(left):
+        result.extend(left[left_idx:])
+    if right_idx < len(right):
+        result.extend(right[right_idx:])
+    return result
+    
